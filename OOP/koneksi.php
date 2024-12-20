322c4cacdc3d618fe8b4f0cb3a5cea0f07c67f27
JunandaDeyastusesa <46320087+JunandaDeyastusesa@users.noreply.github.com>
2024-12-21 00:53:00 +0700

    Update koneksi.php

--- OOP/koneksi.php
+++ OOP/koneksi.php
@@ -10,8 +10,10 @@ class koneksi
   public function __construct()
   {
     $this->koneksi = new mysqli($this->hostname, $this->username, $this->password, $this->name);
-    if ($this->koneksi == false) {
-      die("Koneksi dengan database gagal: " . mysqli_connect_errno() . " - " . mysqli_connect_error());
+    if ($this->koneksi->connect_error) {
+      die("Koneksi dengan database gagal: " . $this->koneksi->connect_error);
+    } else {
+      echo "Koneksi berhasil!";
     }
   }
 }
